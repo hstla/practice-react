44202cde50a0d4d460a504511c52cee08da9a674
hstla <ree7243@gmail.com>
2024-03-27 22:19:20 +0900

    헤더 완료

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
-  return (
-      <div>
-        <Nav />
-      </div>
-)};
+    return (
+        <div className="app">
+            <Nav />
+        </div>
+    );
+}
+
 export default App;
--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,18 +1,21 @@
+// Nav.js
 import React from 'react';
 import './Nav.css';
 
 export default function Nav() {
-    return <Nav className={'nav'}>
-        <img
-            alt="Netflix logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-            className="nav__logo"
-            onClick={() => window.location.reload()}
-        />
-        <img
-            alt="User logged"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-            className="nav__avatar"
-        />
-    </Nav>
+    return (
+        <nav className="nav">
+            <img
+                alt="Netflix logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                className="nav__logo"
+                onClick={() => window.location.reload()}
+            />
+            <img
+                alt="User logged"
+                src={`${process.env.PUBLIC_URL}/my-icon.png`}
+                className="nav__avatar"
+            />
+        </nav>
+    );
 }
