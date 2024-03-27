37b0d91c2f2be7c47b72c01d6076462a0661ea2b
hhwang <hhwang@magicsoft.co.kr>
2024-03-27 17:55:14 +0900

    todo 오류 수정

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Nav.css';
 
 export default function Nav() {
-    return <div></div>
+    return <Nav className={'nav'}>
+        <img
+            alt="Netflix logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            className="nav__logo"
+            onClick={() => window.location.reload()}
+        />
+        <img
+            alt="User logged"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            className="nav__avatar"
+        />
+    </Nav>
 }
