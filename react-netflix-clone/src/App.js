d85120c04411151617ad70fe9379a091ef0188d9
hhwang <hhwang@magicsoft.co.kr>
2024-03-28 10:28:22 +0900

    complete netflex banner

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Banner from './components/Banner';
 
 function App() {
     return (
         <div className="app">
             <Nav />
+            <Banner />
         </div>
     );
 }
